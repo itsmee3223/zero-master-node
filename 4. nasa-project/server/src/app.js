c773258598e4bdc883018bd45e555261a26fab83
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-01 14:12:19 +0700

    add morgan for logging server request

--- 4. nasa-project/server/src/app.js
+++ 4. nasa-project/server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 
 const plantesRouter = require("./routes/planets/planets.router");
 const app = express();
@@ -10,14 +11,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
-
+// middleware
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 // router
+app.use("/planets", plantesRouter);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-app.use("/planets", plantesRouter);
 
 module.exports = app;
