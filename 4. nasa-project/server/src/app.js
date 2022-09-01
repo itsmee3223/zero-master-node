9386f9e07ded6ed23831edba0f8d8f2365849694
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-01 14:01:27 +0700

    production package.json setup

--- 4. nasa-project/server/src/app.js
+++ 4. nasa-project/server/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const plantesRouter = require("./routes/planets/planets.router");
 const app = express();
 
@@ -8,9 +10,14 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 // router
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 app.use("/planets", plantesRouter);
 
 module.exports = app;
